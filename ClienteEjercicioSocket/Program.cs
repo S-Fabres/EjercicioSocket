287eccead5c4671acfbcb97d6f8868fd1c1d45a1
usuario <a@a.com>
2022-07-08 18:45:17 -0400

    Agregar archivos de proyecto.

---
+++ ClienteEjercicioSocket/Program.cs
@@ -0,0 +1,65 @@
+﻿using ClienteEjercicioSocket.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteEjercicioSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            string servidor = ConfigurationManager.AppSettings["servidor"];
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Conectado a servidor");
+            ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+
+            if (clienteSocket.Conectar())
+            {
+                Console.WriteLine("Ingrese puerto del servidor:");
+                string puertoEntrante = clienteSocket.Leer();
+                int puertoInt = Convert.ToInt32(puertoEntrante);
+                if (puertoInt == puerto)
+                {
+                    Console.WriteLine("Ingrese IP del servidor:");
+                    string servidorEntrante = Convert.ToString(clienteSocket.Leer());
+
+                    if (servidorEntrante == servidor)
+                    {
+                        string mensaje="";
+                        while (mensaje != "chao")
+                        {
+                            Console.WriteLine("Ingrese un mensaje");
+                            mensaje = clienteSocket.Leer();
+                            Console.WriteLine(mensaje);
+                        }
+                        Console.WriteLine("chao");
+                        clienteSocket.Desconectar();
+                    }
+                    else
+                    {
+                        Console.WriteLine("IP incorrecta");
+                        Console.WriteLine("chao");
+                        clienteSocket.Desconectar();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Puerto incorrecto");
+                    Console.WriteLine("chao");
+                    clienteSocket.Desconectar();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error de comunicación");
+                Console.WriteLine("chao");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjercicioSocket/Program.cs
@@ -0,0 +1,87 @@
+﻿using EjercicioSocket.Comunicaciones;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            string server = ConfigurationManager.AppSettings["servidor"];
+            Console.WriteLine("Iniciando Servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                Console.WriteLine("Servidor iniciado");
+                while (true)
+                {
+                    Console.WriteLine("Esperando Cliente...");
+                    Socket socketCliente = servidor.ObtenerCliente();
+                    ClienteCom cliente = new ClienteCom(socketCliente);
+                    cliente.Escribir("Ingrese puerto del servidor:");
+                    string puertoEntrante = cliente.Leer();
+                    if (puertoEntrante.Length != 0)
+                    {
+                        int puertoInt = Convert.ToInt32(puertoEntrante);
+                        if (puertoInt == puerto)
+                        {
+                            cliente.Escribir("Ingrese IP del servidor:");
+                            string servidorEntrante = cliente.Leer();
+                            if (servidorEntrante.Length != 0)
+                            {
+                                if (servidorEntrante == server)
+                                {
+                                    string mensaje = "";
+                                    while (mensaje != "chao")
+                                    {
+                                        cliente.Escribir("Ingrese un mensaje:");
+                                        mensaje = cliente.Leer();
+                                        cliente.Escribir(mensaje);
+                                    }
+                                    cliente.Escribir("chao");
+                                    cliente.Desconectar();
+                                }
+                                else
+                                {
+                                    cliente.Escribir("IP del servidor incorrecta");
+                                    cliente.Escribir("chao");
+                                    cliente.Desconectar();
+                                }
+                            }
+                            else
+                            {
+                                cliente.Escribir("Campo vacío");
+                                cliente.Escribir("chao");
+                                cliente.Desconectar();
+                            }
+                        }
+                        else
+                        {
+                            cliente.Escribir("Puerto incorrecto");
+                            cliente.Escribir("chao");
+                            cliente.Desconectar();
+                        }
+                    }
+                    else
+                    {
+                        cliente.Escribir("Campo vacío");
+                        cliente.Escribir("chao");
+                        cliente.Desconectar();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error, el puerto {0} esta en uso...", puerto);
+            }
+        }
+    }
+}
